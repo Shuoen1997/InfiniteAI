23dc323250796994f33ef5c7fff5695df8b534a8
Tyler <tfrick@ucsc.edu>
2018-12-12 20:31:32 -0800

    added 4 obstacle checks

--- checks.py
+++ checks.py
@@ -1,9 +1,40 @@
 
-def is_obstacle_in_way(player_pos):
+#check 10 units ahead
+def is_10obstacle_in_way(player_pos):
+    print("in_is_10obstacle_in_way")
     for i in range(0,10):
         level = player_pos[1]
         if level[player_pos[0]][player_pos[2] + i] is "x":
             print("obstacle detected!")
             return True
-    #print("no obstacle detected")
+    return False
+
+#check 20 units ahead
+def is_20obstacle_in_way(player_pos):
+    print("in_is_20obstacle_in_way")
+    for i in range(0,20):
+        level = player_pos[1]
+        if level[player_pos[0]][player_pos[2] + i] is "x":
+            print("obstacle detected!")
+            return True
+    return False
+
+#check 15 units ahead
+def is_15obstacle_in_way(player_pos):
+    print("in_is_15obstacle_in_way")
+    for i in range(0,15):
+        level = player_pos[1]
+        if level[player_pos[0]][player_pos[2] + i] is "x":
+            print("obstacle detected!")
+            return True
+    return False
+
+#check 5 units ahead
+def is_5obstacle_in_way(player_pos):
+    print("in_is_5obstacle_in_way")
+    for i in range(0,5):
+        level = player_pos[1]
+        if level[player_pos[0]][player_pos[2] + i] is "x":
+            print("obstacle detected!")
+            return True
     return False
